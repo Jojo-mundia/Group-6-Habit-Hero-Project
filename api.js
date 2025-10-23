0258e09cdcb85e4556e1fd091be6786b2241079a
+ <+>
2025-10-23 22:41:00 +0300

    add update and delete API functions (needs testing)

--- api.js
+++ api.js
@@ -5,3 +5,6 @@ const API_URL = "http://localhost:4000";
 export const fetchHabits = () => axios.get(`${API_URL}/habits`);
 export const addHabit = (habitData) =>
   axios.post(`${API_URL}/habits`, habitData);
+export const updateHabit = (id, updatedData) =>
+  axios.post(`${API_URL}/habits/${id}`, updatedData);
+export const deleteHabit = (id) => axios.post(`${API_URL}/habits/${id}`);
