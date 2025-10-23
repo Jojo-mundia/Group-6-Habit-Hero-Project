84006d72746a33613736bca480cc7553e4f163aa
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-10-24 01:29:02 +0300

    clock and home links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,4 +10,23 @@ const Navbar = () => {
     if (hour < 21) return "Evening";
     return "Night";
   };
+
+  return (
+    <nav className="navbar navbar-dark bg-dark p-3"
+    style={{height: "100px"}}>
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <h1>
+          Good {getGreeting()},{user?.firstName}!
+        </h1>
+        <div className="d-flex align-items-center">
+          <Clock />
+          <div className="ms-3">
+            <Link className="btn btn-outline-light me-2 " to="/home">
+            Home
+              </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 };
