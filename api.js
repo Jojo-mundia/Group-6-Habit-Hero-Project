53bfbbe09b982772b40fb6c9fd3d97a75c536c56
+ <+>
2025-10-23 22:41:58 +0300

    correct HTTP methods for update (PUT) and delete (DELETE)

--- api.js
+++ api.js
@@ -6,5 +6,5 @@ export const fetchHabits = () => axios.get(`${API_URL}/habits`);
 export const addHabit = (habitData) =>
   axios.post(`${API_URL}/habits`, habitData);
 export const updateHabit = (id, updatedData) =>
-  axios.post(`${API_URL}/habits/${id}`, updatedData);
-export const deleteHabit = (id) => axios.post(`${API_URL}/habits/${id}`);
+  axios.put(`${API_URL}/habits/${id}`, updatedData);
+export const deleteHabit = (id) => axios.delete(`${API_URL}/habits/${id}`);
