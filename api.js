55da0c051082e5467b34fda09179bd29581424a4
+ <+>
2025-10-23 22:42:53 +0300

    read API_URL from environment variables with fallback

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export const fetchHabits = () => axios.get(`${API_URL}/habits`);
 export const addHabit = (habitData) =>
