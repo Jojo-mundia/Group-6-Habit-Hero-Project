fe8512b4692c7cd8e00d5f9150e1291a69bc6ec9
+ <+>
2025-10-24 09:45:27 +0300

    prepare AddHabitForm to receive onAddHabit and userId props

--- src/components/AddHabitForm.jsx
+++ src/components/AddHabitForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const AddHabitForm = () => {
+const AddHabitForm = ({ onAddHabit, userId }) => {
   const [name, setName] = useState("");
 
   const generateWeek = () => {
