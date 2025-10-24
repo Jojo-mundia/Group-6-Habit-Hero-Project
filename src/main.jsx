69244f3655d264b53347e3542cd6b2af114bfd52
+ <+>
2025-10-24 09:40:18 +0300

    integrate ClerkProvider (testing env variable)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const CLERK_KEY = import.meta.env.VITE_CLERK_API_KEY;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={CLERK_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
